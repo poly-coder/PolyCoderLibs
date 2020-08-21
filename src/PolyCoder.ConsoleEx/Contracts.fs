ab30ea4cb88b70e2564b7b392036958fc416945a
Iskander Sierra <iskander.sierra@gmail.com>
2020-08-21 22:31:08 +0200

    Types and Contracts from Konsole

---
+++ src/PolyCoder.ConsoleEx/Contracts.fs
@@ -0,0 +1,79 @@
+﻿namespace PolyCoder.ConsoleEx.Contracts
+
+// Inspired by https://github.com/goblinfactory/konsole
+
+open System
+open System.Runtime.InteropServices
+open PolyCoder.ConsoleEx
+
+type ISetColors =
+  abstract member ForegroundColor: ConsoleColor with get, set
+  abstract member BackgroundColor: ConsoleColor with get, set
+  
+  abstract member Colors: Colors with get, set
+
+type IPrintAt =
+  abstract member PrintAt: x: int * y: int * format: string * [<ParamArray>] args: obj[] -> unit
+  abstract member PrintAt: x: int * y: int * text: string -> unit
+  abstract member PrintAt: x: int * y: int * c: char -> unit
+
+type IPrintAtColor =
+  inherit IPrintAt
+  inherit ISetColors
+
+  abstract member PrintAtColor: foreground: ConsoleColor * x: int * y: int * text: string * ?background: ConsoleColor -> unit
+
+type IConsoleState =
+  inherit ISetColors
+
+  abstract member State: ConsoleState with get, set
+  
+  abstract member CursorTop: int with get, set
+  abstract member CursorLeft: int with get, set
+  abstract member CursorVisible: bool with get, set
+
+  abstract member DoCommand: console: IConsoleState * action: Action -> unit;
+
+type IWrite =
+  abstract member WriteLine: format: string * [<ParamArray>] args: obj[] -> unit
+  abstract member WriteLine: text: string -> unit
+  abstract member Write: format: string * [<ParamArray>] args: obj[] -> unit
+  abstract member Write: text: string -> unit
+  abstract member Clear: unit -> unit
+
+type IWriteColor =
+  inherit IWrite
+  inherit ISetColors
+
+  abstract member WriteLine: color: ConsoleColor * format: string * [<ParamArray>] args: obj[] -> unit
+  abstract member WriteLine: color: ConsoleColor * text: string -> unit
+  abstract member Write: color: ConsoleColor * format: string * [<ParamArray>] args: obj[] -> unit
+  abstract member Write: color: ConsoleColor * text: string -> unit
+  abstract member Clear: ?backgroundColor: ConsoleColor -> unit
+
+type IWindowed =
+  abstract member AbsoluteX: int with get
+  abstract member AbsoluteY: int with get
+
+  abstract member WindowWidth: int with get
+  abstract member WindowHeight: int with get
+
+type IScrolling =
+  abstract member MoveBufferArea: sourceLeft: int * sourceTop: int * sourceWidth: int * sourceHeight: int * targetLeft: int * targetTop: int * sourceChar: char * sourceForeColor: ConsoleColor * sourceBackColor: ConsoleColor -> unit
+  abstract member ScrollDown: unit -> unit
+
+type IScrollingWindow =
+  inherit IWindowed
+  inherit IScrolling
+
+type IConsole =
+  inherit IPrintAtColor
+  inherit IConsoleState
+  inherit IWriteColor
+  inherit IScrollingWindow
+
+type IKeyboard =
+  abstract member ReadKey: [<DefaultParameterValue(false)>] ?intercept: bool -> ConsoleKeyInfo
+  abstract member WaitForKeyPress: [<ParamArray>] chars: char[] -> unit
+  abstract member OnCharPressed: chars: char[] * key: Action<char> -> unit
+  abstract member OnCharPressed: c: char * key: Action<char> -> unit
---
+++ src/PolyCoder.ConsoleEx/Types.fs
@@ -0,0 +1,29 @@
+﻿namespace PolyCoder.ConsoleEx
+
+open System
+
+type Colors(foreground: ConsoleColor, background: ConsoleColor) =
+
+  member val Foreground = foreground with get
+  member val Background = background with get
+  
+  new () = Colors(ConsoleColor.Gray, ConsoleColor.Black)
+
+  static member val WhiteOnBlack = Colors(ConsoleColor.Gray, ConsoleColor.Black)
+  static member val BlackOnWhite = Colors(ConsoleColor.Black, ConsoleColor.Gray)
+
+type ConsoleState
+  (
+    foreground: ConsoleColor,
+    background: ConsoleColor,
+    top: int,
+    left: int,
+    cursorVisible: bool
+  ) =
+
+  member val Foreground = foreground with get, set
+  member val Background = background with get, set
+  member val Top = top with get, set
+  member val Left = left with get, set
+  member val CursorVisible = cursorVisible with get, set
+
